63350ae067b8fd78aca793820fc765e079326550
Noki-Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2026-05-31 01:00:37 +0700

    chore: bump version to 6.4.2

--- bundle-extension.js
+++ bundle-extension.js
@@ -41,7 +41,7 @@ let manifest = {
   name: "Minimal Theme for Twitter / X",
   short_name: "Minimal Twitter",
   description: "Refine and declutter the 𝕏/Twitter web experience.",
-  version: "6.4.1",
+  version: "6.4.2",
   icons: {
     16: "images/MinimalTwitterIcon16.png",
     32: "images/MinimalTwitterIcon32.png",
